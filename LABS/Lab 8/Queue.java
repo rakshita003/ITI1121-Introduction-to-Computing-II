dc214cedcd69400ab489f09f19b6e79193313f6f
Rakshita Mathur <43514952+rakshita003@users.noreply.github.com>
2021-03-23 10:20:33 +0530

    Update Queue.java

--- LABS/Lab 8/Queue.java
+++ LABS/Lab 8/Queue.java
@@ -1,10 +1,3 @@
-// -*- Mode: Java -*- 
-// Queue.java --- Queue interface
-// Author          : Marcel Turcotte
-// Created On      : Sat Mar  3 07:59:16 2007
-// Last Modified By: Marcel Turcotte
-// Last Modified On: Sat Mar  3 07:59:28 2007
-// ITI 1121/1521. Introduction to Computer Science II
 
 public interface Queue<E> {
 
