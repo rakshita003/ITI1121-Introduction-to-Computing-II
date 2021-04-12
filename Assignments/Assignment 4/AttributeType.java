4fe357c912388a9698ee7338f17ee13a5452a2a6
Rakshita Mathur <43514952+rakshita003@users.noreply.github.com>
2021-04-12 18:28:25 +0530

    Update AttributeType.java

--- Assignments/Assignment 4/AttributeType.java
+++ Assignments/Assignment 4/AttributeType.java
@@ -1,8 +1,5 @@
 /**
  * Enumeration class for attribute types
- * 
- * @author Mehrdad Sabetzadeh, University of Ottawa
- * @author Guy-Vincent Jourdan, University of Ottawa
  *
  */
 public enum AttributeType {
