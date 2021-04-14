a587353403cdf4f0650fcb9e5ea74add0636ef45
Rakshita Mathur <43514952+rakshita003@users.noreply.github.com>
2021-04-14 09:51:27 +0530

    Update UniquifiableArrayStack.java

--- Midterm/UniquifiableArrayStack.java
+++ Midterm/UniquifiableArrayStack.java
@@ -1,10 +1,3 @@
-/**
- * COPYRIGHTED MATERIAL -- DO NOT DISTRIBUTE
- *
- * @author Guy-Vincent Jourdan
- * @author Mehrdad Sabetzadeh 
- */
-
 public class UniquifiableArrayStack<E> extends ArrayStack<E> {
 
 	public Stack<E> uniquify() {
